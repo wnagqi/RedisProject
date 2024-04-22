cd20bdcf3a963dff7f4f4d7a9d4d361948748c10
wangqi0324 <qiwang0324@163.com>
2024-04-23 00:06:06 +0800

    redis基本操作

---
+++ RedisProject/src/main/java/com/wangqi/pojo/User.java
@@ -0,0 +1,18 @@
+package com.wangqi.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author wangqi
+ * @Date 2024/4/22 22:48
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String userName;
+    private Integer age;
+
+}
--- RedisProject/src/test/java/com/wangqi/RedisProjectApplicationTest.java
+++ RedisProject/src/test/java/com/wangqi/RedisProjectApplicationTest.java
@@ -1,10 +1,14 @@
 package com.wangqi;
 
 
+import com.alibaba.fastjson2.JSON;
+import com.wangqi.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.Map;
 
 /**
  * Unit test for simple App.
@@ -12,13 +16,35 @@ import org.springframework.data.redis.core.RedisTemplate;
 @SpringBootTest
 public class RedisProjectApplicationTest {
     @Autowired
-    private RedisTemplate redisTemplate;
+    private StringRedisTemplate redisTemplate;
 
     @Test
     void testString(){
+
         redisTemplate.opsForValue().set("name","王琦");
         Object result = redisTemplate.opsForValue().get("name");
         System.out.println("result = " + result);
 
+    }
+
+    @Test
+    void testUserJson(){
+        User user = new User("王琦", 27);
+        // 序列化
+        String userJson = JSON.toJSONString(user);
+        redisTemplate.opsForValue().set("user:wq",userJson);
+        String userJsonString = redisTemplate.opsForValue().get("user:wq");
+        // 反序列化
+        User userObject = JSON.parseObject(userJsonString, User.class);
+        System.out.println("userObject = " + userObject);
+
+    }
+
+    @Test
+    void testHash(){
+        redisTemplate.opsForHash().put("user:100","name","王琦");
+        redisTemplate.opsForHash().put("user:100","age","27");
+        Map<Object, Object> entries = redisTemplate.opsForHash().entries("user:100");
+        System.out.println("entries = " + entries);
     }
 }
